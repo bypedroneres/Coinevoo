40424d60edd47dad6f65abd9a4ff4c50d7150362
Pedro Neres <bypedroneres@gmail.com>
2023-10-24 17:00:18 -0300

    new dashboard

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,18 @@
-document.getElementById("about-link").addEventListener("click", function() {
-    const aboutSection = document.querySelector(".about-section");
-    aboutSection.scrollIntoView({ behavior: "smooth" });
+$(document).ready(function() {
+    // When the profile picture is clicked, trigger the file input click
+    $("#profile-picture").click(function() {
+        $("#profile-picture-upload").click();
+    });
+
+    // When a new image is selected, update the background image of the .profile-picture div
+    $("#profile-picture-upload").change(function() {
+        var selectedFile = this.files[0];
+
+        if (selectedFile) {
+            var imageUrl = URL.createObjectURL(selectedFile);
+
+            // Update the background image of the .profile-picture div
+            $(".profile-picture").css("background-image", "url(" + imageUrl + ")");
+        }
+    });
 });
