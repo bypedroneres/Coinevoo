c6388868601ac4156fbf03da751e44d9476a3b23
vitoria freire <vitoria.loruama.freire@gmail.com>
2023-10-25 10:27:18 -0300

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // When the profile picture is clicked, trigger the file input click
-    $("#profile-picture").click(function() {
+    $("#profile-picture").click(function () {
         $("#profile-picture-upload").click();
     });
 
     // When a new image is selected, update the background image of the .profile-picture div
-    $("#profile-picture-upload").change(function() {
+    $("#profile-picture-upload").change(function () {
         var selectedFile = this.files[0];
 
         if (selectedFile) {
